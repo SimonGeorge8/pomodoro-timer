f7e79ceb970a64d2fced23fb0389cdfae9913cd1
Simon Abadi <Simon.george986@gmail.com>
2024-10-29 11:17:43 -0500

    Mockup of general structure of the web as implemented with Daisy UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import './App.css';
+import React, { useState } from 'react';
 
-
-
-function Timer() {
-
-}
-
-function toDoList() {
+const Timer = () => {
+  const [seconds , setSeconds] = useState(0);
+  const [minutes , setMinutes] = useState(0);
   
+
+    return(
+      <div className="flex justify-center">
+      <div className="card glass w-96">
+        <div className="card-body">
+          <h2 className="card-title">Time Remaining</h2>
+          <div className="text-center">
+            <span className="text-6xl font-bold">10:00</span>
+          </div>
+          <div className="card-actions justify-center mt-4">
+            <button className="btn btn-primary">Reset Timer</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
 }
+
+const ToDoList = () => {
+
+};
 
 
 
@@ -16,7 +33,9 @@ function toDoList() {
 
 
 function App() {
-
+    return(
+      <Timer />
+    );
 }
 
 export default App;
